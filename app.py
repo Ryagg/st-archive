15e433416f9cd7eb60492c1dd9ed2b17b1aff91a
Ryagg <58631455+Ryagg@users.noreply.github.com>
2021-10-09 23:22:20 +0200

    feat: add login template, update links in base template, add login functionality

--- app.py
+++ app.py
@@ -56,9 +56,37 @@ def register():
 
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
-        # flash("Registration Successful!")
+        flash(f'Registration successful! Welcome to ST-Archive, {request.form.get("username")}')
         # return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """sumary_line"""
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash(f'Welcome, {request.form.get("username")}!')
+                return redirect(url_for("index", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 @app.route("/copyrights")
