7853a3f9f506a3cb08df3bd30d94902fa10505e1
Ryagg <58631455+Ryagg@users.noreply.github.com>
2022-04-12 22:23:05 +0200

    test: Add test case for basic_series_search function

---
+++ tests/test_search.py
@@ -0,0 +1,12 @@
+import pytest
+
+def test_basic_series_search() -> None:
+# Given the Memory Alpha (or Beta) novels page for a selected series
+# is displayed
+
+# The book titles are displayed as list elements beneath a h2 'Novels'
+
+# The linked pages contain: the book title, the series code, the ISBN numbers
+# for the available formats, the blurb, the book cover, and the publication
+# date
+    pass
